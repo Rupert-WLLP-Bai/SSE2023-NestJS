392c6869f640d8bb6b18b225b6b6f7b460ac75dd
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2023-01-04 13:42:42 +0800

    [Fix 1] [user] [controller] async/await

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
 import {
+  QueryResponse,
+  UpdateResponse,
+} from 'src/common/response/response.interface';
+import {
+  Response,
+  DeleteResponse,
+} from './../common/response/response.interface';
+import {
   Controller,
   Get,
   Post,
@@ -12,7 +20,13 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBody, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('user')
 @Controller('user')
@@ -28,45 +42,65 @@ export class UserController {
 
   @Get(':id')
   @ApiOperation({ summary: '根据id查询用户' })
-  findOne(@Param('id') id: string) {
+  @ApiParam({ name: 'id', description: '用户id' })
+  async findOne(@Param('id') id: string): Promise<QueryResponse> {
     // 日志输出参数
     Logger.log('[GET] /user/:id', id);
-    // 单条记录返回格式
-    // {
-    //   "success": true,
-    //   "data": {},
-    //   "message": "查询成功"
-    // }
-    const result = {
+    const result: QueryResponse = {
       success: true,
       data: {},
-      message: '查询成功',
+      errorCode: '',
+      errorMessage: '',
+      showType: 0,
+      traceId: '',
+      host: '',
     };
-    result.data = this.userService.findOne(+id);
+    const query_result = await this.userService.findOne(+id);
+    result.data = {
+      list: [query_result],
+      total: query_result ? 1 : 0,
+    };
     return result;
   }
 
   @Patch(':id')
   @ApiOperation({ summary: '根据id更新用户' })
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    const result = {
+  @ApiParam({ name: 'id', description: '用户id' })
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    const result: UpdateResponse = {
       success: true,
-      data: {},
-      message: '更新成功',
+      data: {
+        raw: {},
+        affected: 0,
+        generatedMaps: [],
+      },
+      errorCode: '',
+      errorMessage: '',
+      showType: 0,
+      traceId: '',
+      host: '',
     };
-    result.data = this.userService.update(+id, updateUserDto);
+    result.data = await this.userService.update(+id, updateUserDto);
     return result;
   }
 
   @Delete(':id')
   @ApiOperation({ summary: '根据id删除用户' })
-  remove(@Param('id') id: string) {
-    const result = {
+  @ApiParam({ name: 'id', description: '用户id' })
+  async remove(@Param('id') id: string) {
+    const result: DeleteResponse = {
       success: true,
-      data: {},
-      message: '删除成功',
+      data: {
+        raw: {},
+        affected: 0,
+      },
+      errorCode: '',
+      errorMessage: '',
+      showType: 0,
+      traceId: '',
+      host: '',
     };
-    result.data = this.userService.remove(+id);
+    result.data = await this.userService.remove(+id);
     return result;
   }
 
@@ -76,28 +110,39 @@ export class UserController {
   // page设置为required: false, pageSize设置为required: false
   @Get()
   @ApiOperation({ summary: '分页查询用户' })
-  @ApiQuery({ name: 'page', required: false })
-  @ApiQuery({ name: 'pageSize', required: false })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    description: '页码, 不传则返回所有数据',
+  })
+  @ApiQuery({
+    name: 'pageSize',
+    required: false,
+    description: '每页数量, 不传则返回所有数据',
+  })
   async findPage(
     // 将page和pageSize设置为可选参数
     // 设置swagger文档中的参数为可选
     @Query('page') page?: number,
     @Query('pageSize') pageSize?: number,
   ) {
-    const result = {
+    const result: QueryResponse = {
       success: true,
       data: {},
-      message: '查询成功',
-      total: 0,
+      errorCode: '',
+      errorMessage: '',
+      showType: 0,
+      traceId: '',
+      host: '',
     };
     if (page && pageSize) {
       const data = await this.userService.findPage(page, pageSize);
-      result.data = data[0];
-      result.total = data[1];
+      result.data.list = data[0];
+      result.data.total = data[1];
     } else {
       const data = await this.userService.findAllAndCount();
-      result.data = data[0];
-      result.total = data[1];
+      result.data.list = data[0];
+      result.data.total = data[1];
     }
     return result;
   }
