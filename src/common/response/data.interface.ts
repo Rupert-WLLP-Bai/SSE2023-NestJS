55f9fff7279574d0e26562ef7e46757a1a3cbba2
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2023-01-17 10:27:05 +0800

    (fix) change interface to class

--- src/common/response/data.interface.ts
+++ src/common/response/data.interface.ts
@@ -1,12 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 /**
  * @file 数据接口
  * @description 定义后端响应数据内部的数据接口
  * @author Junhao Bai
  * @date 2023-01-04
  */
-export interface QueryResult {
+export class QueryResult {
+  @ApiProperty({ description: '数据列表', example: [] })
   list?: any[];
+  @ApiProperty({ description: '数据总数', example: 0 })
   total?: number;
+  @ApiProperty({ description: '当前页码', example: 1 })
   current?: number;
+  @ApiProperty({ description: '每页数据量', example: 10 })
   pageSize?: number;
 }
