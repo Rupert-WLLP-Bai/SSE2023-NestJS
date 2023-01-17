a172b952bf2f74d38969f76bb03b7d131d7c2b10
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2023-01-17 10:27:05 +0800

    (fix) change interface to class

--- src/common/response/response.interface.ts
+++ src/common/response/response.interface.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { DeleteResult, UpdateResult } from 'typeorm';
 import { QueryResult } from './data.interface';
 /**
@@ -8,52 +9,101 @@ import { QueryResult } from './data.interface';
  * @version 1.0.0
  * @license MIT
  */
-export interface Response {
+export class NormalResponse {
+  @ApiProperty({ description: '请求是否成功', example: true })
   success: boolean;
+  @ApiProperty({ description: '请求返回的数据', example: {} })
   data?: any;
+  @ApiProperty({ description: '错误码', example: '' })
   errorCode?: string;
+  @ApiProperty({ description: '错误信息', example: '' })
   errorMessage?: string;
+  @ApiProperty({ description: '错误类型', example: 0 })
   showType?: number;
+  @ApiProperty({ description: 'traceId', example: '' })
   traceId?: string;
+  @ApiProperty({ description: 'host', example: '' })
   host?: string;
 }
 
-export interface UpdateResponse {
+export class UpdateResponse {
+  @ApiProperty({ description: '请求是否成功', example: true })
   success: boolean;
+  @ApiProperty({
+    description: '请求返回的数据',
+    example: { raw: [], affected: 1, generatedMaps: [] },
+  })
   data?: UpdateResult;
+  @ApiProperty({ description: '错误码', example: '' })
   errorCode?: string;
+  @ApiProperty({ description: '错误信息', example: '' })
   errorMessage?: string;
+  @ApiProperty({ description: '错误类型', example: 0 })
   showType?: number;
+  @ApiProperty({ description: 'traceId', example: '' })
   traceId?: string;
+  @ApiProperty({ description: 'host', example: '' })
   host?: string;
 }
 
-export interface DeleteResponse {
+export class DeleteResponse {
+  @ApiProperty({ description: '请求是否成功', example: true })
   success: boolean;
+  @ApiProperty({
+    description: '请求返回的数据',
+    example: { raw: [], affected: 1, generatedMaps: [] },
+  })
   data?: DeleteResult;
+  @ApiProperty({ description: '错误码', example: '' })
   errorCode?: string;
+  @ApiProperty({ description: '错误信息', example: '' })
   errorMessage?: string;
+  @ApiProperty({ description: '错误类型', example: 0 })
   showType?: number;
+  @ApiProperty({ description: 'traceId', example: '' })
   traceId?: string;
+  @ApiProperty({ description: 'host', example: '' })
   host?: string;
 }
 
-export interface CreateResponse {
+export class CreateResponse {
+  @ApiProperty({ description: '请求是否成功', example: true })
   success: boolean;
+  @ApiProperty({ description: '请求返回的数据', example: {} })
   data?: any;
+  @ApiProperty({ description: '错误码', example: '' })
   errorCode?: string;
+  @ApiProperty({ description: '错误信息', example: '' })
   errorMessage?: string;
+  @ApiProperty({ description: '错误类型', example: 0 })
   showType?: number;
+  @ApiProperty({ description: 'traceId', example: '' })
   traceId?: string;
+  @ApiProperty({ description: 'host', example: '' })
   host?: string;
 }
 
-export interface QueryResponse {
+export class QueryResponse {
+  @ApiProperty({ description: '请求是否成功', example: true })
   success: boolean;
+  @ApiProperty({
+    description: '请求返回的数据',
+    example: {
+      total: 1,
+      current: 1,
+      pageSize: 10,
+      data: [],
+    },
+  })
   data: QueryResult;
+  @ApiProperty({ description: '错误码', example: '' })
   errorCode?: string;
+  @ApiProperty({ description: '错误信息', example: '' })
   errorMessage?: string;
+  @ApiProperty({ description: '错误类型', example: 0 })
   showType?: number;
+  @ApiProperty({ description: 'traceId', example: '' })
   traceId?: string;
+  @ApiProperty({ description: 'host', example: '' })
   host?: string;
 }
