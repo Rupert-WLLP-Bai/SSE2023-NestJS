00cd577fb79f8d01d194b8600a2dc01f03622c76
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2023-02-02 20:24:57 +0800

    (feat) 2023/02/02 加入了七牛云的查询 上传 下载功能

---
+++ src/common/key.ts
@@ -0,0 +1,13 @@
+/**
+ * @file 七牛云存储的AccessKey和SecretKey
+ */
+
+/**
+ * 七牛云存储的AccessKey
+ */
+export const AccessKey = 'tTKBbCy5QBzvN9nHhRSjhEqiIAQX2vveprrkv1Jo';
+
+/**
+ * 七牛云存储的SecretKey
+ */
+export const SecretKey = 'S43NJSG_iL-g0d_Vsk7Z_YF_onfD4AMDeMVqdmAF';
